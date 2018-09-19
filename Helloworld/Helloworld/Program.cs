b462287739985331dd9e8399273158ac532d5788
lydenlau“ git config --global user.email 529246573@qq.com“ <529246573@qq.comgit config --global user.name lydenlau>
2018-09-19 11:52:03 +0800

    new file

---
+++ Helloworld/Helloworld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1+2={0}", 1 + 2);
+            Console.ReadKey();        
+        }
+    }
+}
