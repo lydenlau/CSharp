119a27a256e41956b33ede7377ec51212cef0b6a
lydenlau“ git config --global user.email 529246573@qq.com“ <529246573@qq.comgit config --global user.name lydenlau>
2018-09-30 20:18:57 +0800

    new file

--- test1/test1/Program.cs
+++ test1/test1/Program.cs
@@ -9,17 +9,51 @@ namespace LCS
     class LCS
     {
         /// <summary>
-        /// b[i,j]记录指示c[i,j]的值是由哪一个子问题的解达到的
-        /// c[i,j]存储Xi与Yj的最长公共子序列的长度
+        /// b[i,j]的作用是记录指示c[i,j]的值是由哪一个子问题的解达到的
+        /// c[i,j]的作用是存储Xi与Yj的最长公共子序列的长度
         /// </summary>
-        string[,] b;        //定义的存储字符串的b数组
-        int[,] c;           //定义的存放矩阵空间的数值
+        string[,] b;        //定义b数组用于存储字符串
+        int[,] c;           //定义的存放矩阵的空间
         char a;
+
+        static void Main(string[] args)   //程序从main函数开始运行
+        {
+            int[] L1 = { 34, 72, 13, 44, 25, 30, 10 };   //自定义的数组
+            int[] L2 = { 34, 13, 44, 7, 25 };
+            string[] M = { "我", "的", "名字", "是", "刘", "一", "达" };  //自定义的对象
+            string[] N = { "刘", "一达", "是", "个", "好", "名字" };
+            LCS lcs = new LCS();     //实例化一个lcs对象
+            lcs.LCS_LENGTH(L1, L2);   //整形情况
+            for (int i = 0; i < L1.Length; i++)     //遍历这个矩阵依次打印回溯左上角的元素
+            {
+                for (int j = 0; j < L2.Length; j++)
+                {                  
+                    if (lcs.b[i, j] == "left_up")
+                    {
+                        Console.WriteLine("{0}", L1[i]);
+                    }                   
+                }
+            }
+            lcs.LCS_LENGTH1(M, N);    //字符串情况
+            for (int i = 0; i < M.Length; i++)
+            {
+                for (int j = 0; j < N.Length; j++)
+                {                    
+                    if (lcs.b[i, j] == "left_up")
+                    {
+                        Console.WriteLine("{0}", M[i]);
+                    }
+
+                }
+
+            }
+            Console.ReadLine();
+        }
 
         public void LCS_LENGTH(int[] X, int[] Y)
         {
-            b = new string[X.Length, Y.Length];         //存放← ↑ ↖
-            c = new int[X.Length + 1, Y.Length + 1];    //位于↖右下角元素用于回溯，从而找出公共数值
+            b = new string[X.Length, Y.Length];         //存放左/上/左上
+            c = new int[X.Length + 1, Y.Length + 1];    //位于左上右下角元素用于回溯，从而找出公共数值
             for (int i = 0; i <= X.Length; i++)
             {
                 c[i, 0] = 0;//j=0,c[i,j]=0;表示最长公共子序列的长度为0
@@ -36,29 +70,28 @@ namespace LCS
                     {
                         a = ' ';
                         c[i + 1, j + 1] = c[i, j] + 1;
-                        b[i, j] = "left_up";  //表示向左上回溯↖
+                        b[i, j] = "left_up";  //表示向左上回溯
                     }
                     else if (c[i, j + 1] >= c[i + 1, j])
                     {
                         a = '+';
                         c[i + 1, j + 1] = c[i, j + 1];
-                        b[i, j] = "up";  //表示向上回溯↑
+                        b[i, j] = "up";  //表示向上回溯
                     }
                     else
                     {
                         a = '+';
                         c[i + 1, j + 1] = c[i + 1, j];
-                        b[i, j] = "left";  //表示向左回溯←
+                        b[i, j] = "left";  //表示向左回溯
                     }
 
                 }
             }
         }
-
         public void LCS_LENGTH1(string[] M, string[] N)
         {
-            b = new string[M.Length, N.Length];         //存放← ↑ ↖
-            c = new int[M.Length + 1, N.Length + 1];    //位于↖右下角元素用于回溯，从而找出公共数值
+            b = new string[M.Length, N.Length];         //存放左/上/左上
+            c = new int[M.Length + 1, N.Length + 1];    //位于左上右下角元素用于回溯，从而找出公共数值
             for (int i = 0; i <= M.Length; i++)
             {
                 c[i, 0] = 0;//j=0,c[i,j]=0;表示最长公共子序列的长度为0
@@ -74,17 +107,17 @@ namespace LCS
                     if (M[i] == N[j])
                     {
                         c[i + 1, j + 1] = c[i, j] + 1;
-                        b[i, j] = "left_up";  //表示向左上回溯↖
+                        b[i, j] = "left_up";  //表示向左上回溯
                     }
                     else if (c[i, j + 1] >= c[i + 1, j])
                     {
                         c[i + 1, j + 1] = c[i, j + 1];
-                        b[i, j] = "up";  //表示向上回溯↑
+                        b[i, j] = "up";  //表示向上回溯
                     }
                     else
                     {
                         c[i + 1, j + 1] = c[i + 1, j];
-                        b[i, j] = "left";  //表示向左回溯←
+                        b[i, j] = "left";  //表示向左回溯
                     }
 
                 }
@@ -110,56 +143,11 @@ namespace LCS
                 LCSW(a, b, X, i, j - 1);
             }
         }
-
         // public void symbol(char a)
         //  {
         //    LCS.LCS_LENGTH(X,Y);
         // }
-        static void Main(string[] args)   //程序从main函数开始运行
-        {
-            int[] L1 = { 14, 56, 13, 44, 25, 30, 10, 7 };   //自定义的数组
-            int[] L2 = { 14, 13, 44, 7, 25 };
-            string[] M = { "我", "不是", "名", "一个", "侦", "探", "柯", "南" };  //自定义的对象
-            string[] N = { "名", "侦", "探", "柯", "南", "小胖" };
-            LCS lcs = new LCS();     //实例化一个lcs对象
-            lcs.LCS_LENGTH(L1, L2);   //运用lcs类中的方法
-            for (int i = 0; i < L1.Length; i++)     //遍历这个矩阵依次打印回溯左上角的元素
-            {
-                for (int j = 0; j < L2.Length; j++)
-                {
-
-                    // lcs.LCSW(lcs.a,lcs.b, L1, i, j);
-                    //   Console.WriteLine("{0}", L1[i]);
-                    if (lcs.b[i, j] == "left_up")
-                    {
-                        Console.WriteLine("{0}", L1[i]);
-                    }
-                    //    else if (lcs.b[i, j] == "up")
-                    //       Console.WriteLine("{0}", L1[i]);
-                    //  else
-                    //     Console.WriteLine("{0}", L1[i]);
-
-
-
-
-                }
-            }
-            lcs.LCS_LENGTH1(M, N);    //同理
-            for (int i = 0; i < M.Length; i++)
-            {
-                for (int j = 0; j < N.Length; j++)
-                {
-                    //lcs.LCSP(lcs.b, list1, i, j);
-                    if (lcs.b[i, j] == "left_up")
-                    {
-                        Console.WriteLine("{0}", M[i]);
-                    }
-
-                }
-
-            }
-            Console.ReadLine();
-        }
+       
     }
 }
 
